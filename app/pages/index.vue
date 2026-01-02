22f4d2449a7f72955d4eeee01b2f21b590ad4dd3
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2026-01-02 04:30:31 -0800

    Remove graph setup from index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,27 +11,6 @@
 import { ref, onMounted } from "vue";
 import GraphViewer from "@@/components/GraphViewer.vue";
 import PathSidebar from "@@/components/PathSidebar.vue";
-
-const graph = ref({ nodes: [], links: [] });
-
-onMounted(async () => {
-  try {
-    // seed graph with some popular players or sample
-    const res = await $fetch("/api/player/76561198936168937").catch(() => null);
-
-    if (res && res.found) {
-      graph.value.nodes = [
-        {
-          id: res.player.id,
-          name: res.player.rglName || res.player.etf2lName || res.player.id,
-          type: "player",
-        },
-      ];
-    }
-  } catch (err) {
-    console.error(err);
-  }
-});
 </script>
 
 <style scoped>
